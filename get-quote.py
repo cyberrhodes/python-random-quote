c2833a2971c0245bc981b5eeb08e895de6e896b6
Anthony Rhodes <starcaster@cyberrhodes.com>
2021-04-05 17:17:56 -0700

    completed all challenges, now moving to next project

--- get-quote.py
+++ get-quote.py
@@ -1,5 +1,5 @@
 import random
-
+import os,sys 
 
 
 
@@ -7,12 +7,24 @@ def primary():
   # print("Keep it logically awesome.")
     # print("Well hi there, gotta keep it going!.")
   f = open("quotes.txt")
-  quotes = f.readlines()
+  quote1 = f.readlines()
   f.close()
+  g = open("quotes.txt")
+  quote2 = g.readlines()
+  g.close
+  
+  frst =13
+  rad=random.randint(0, frst)   
   last = 13
   rnd=random.randint(0, last)
+  
+  print(" " + quote1[rad], end=" " )
+  print(quote2[rnd], end=" " )
+  #print(quote2[rnd])
+  e = open('testtext.txt', 'w')
+  print (quote1[rad], quote2[rnd], file=e)
+  e.close
 
-  print(quotes[rnd])
 
 if __name__== "__main__":
   primary()
